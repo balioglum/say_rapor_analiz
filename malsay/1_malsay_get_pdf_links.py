7478d7a84296cee32d42f0d4e3f63bbf4ff3edbc
mehmet <balioglum@gmail.com>
2022-08-06 13:47:04 +0200

    .

---
+++ malsay/1_malsay_get_pdf_links.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+import time
+import pandas as pd
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import re
+from pprint import pprint
+
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--profile-directory=Default')
+chrome_options.add_argument('--user-data-dir=~/.config/google-chrome')
+
+browser = webdriver.Chrome(options=chrome_options)
+top_html="https://www.sayistay.gov.tr/reports/category/31-belediyeler---bagli-idareler"
+browser.get(top_html)
+
+html = browser.page_source
+#print(html)
+links=[]
+kurum=[]
+yil=[]
+
+cols = ['KURUM', 'YIL', 'LINK']
+
+elems = browser.find_elements(by=By.XPATH, value='//a[@href]')
+for elem in elems:
+    
+    if re.match(r"https:\/\/www\.sayistay\.gov\.tr\/reports\/[0-9]{4}.*", str(elem.get_attribute("href"))):
+        #attrs = browser.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', elem)
+        #pprint(attrs)
+        e=elem.get_attribute("href").strip()
+        first_part = e[0:36]
+        last_part=e[36:]
+        rapor_link=f"{first_part}download/{last_part}"
+        print(f"{first_part}download/{last_part}")
+        links.append(rapor_link)
+        
+        kurumun_ismi=elem.get_attribute("text").strip()
+        kurum_ad, rapor_yil = kurumun_ismi[:-4].strip(), kurumun_ismi[-4:].strip()
+        kurum.append(kurum_ad)
+        yil.append(rapor_yil)
+        
+        print(f"{kurum_ad}_{rapor_yil}")
+        
+data_tuples = list(zip(kurum,yil,links))
+#time.sleep(5)
+df = pd.DataFrame(data_tuples, columns=cols)
+
+print(df)
+# with open(r'/root/malsay/raporlinks.csv', 'w') as fp:
+#     fp.write('\n'.join(links))
+
+df.to_csv('kurum_links.csv',index=False, header=True)
+
+
+
